f5dc9d9d995ed714d4917d01c823eed2ec2d4b82
RidhamRj <singhal2003yash@gmail.com>
2025-07-06 16:48:06 +0530

    hero sec fixed

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
     return (
-        <div className='bg-black text-white flex items-center justify-around h-12 w-[50%] fixed top-10 rounded-3xl border-white border py-8'>
+        <div className='bg-black text-white flex items-center justify-around h-12 w-[50%] fixed top-10 rounded-3xl border-white border py-8 z-20'>
             <div className='text-2xl font-bold'>Portfolio</div>
             <div className='flex space-x-4'>
                 <a href="#home" className='hover:text-gray-400'>Home</a>
